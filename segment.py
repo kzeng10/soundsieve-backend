1b03b260fe93d626ac0c699a5d89b62a08aad0c7
kzeng10 <kzeng10@gmail.com>
2015-02-14 19:30:05 -0800

    Added update if too old
    Also moved client ID and secret to uncommitted file :P and default sort
    set to hot

--- segment.py
+++ segment.py
@@ -12,6 +12,7 @@ import random
 import math
 from google.appengine.ext import db
 from google.appengine.api import memcache
+from secret import client_id, client_secret
 
 template_dir = os.path.dirname(__file__)
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
@@ -21,10 +22,7 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 #https://api-v2.soundcloud.com/explore/techno?limit=100&linked_partitioning=1
 # stream url: https://api.soundcloud.com/tracks/189594894/stream?client_id=6ec16ffb5ed930fce00949be480f746b&allows_redirect=false#t=50
 # comment url: https://api.soundcloud.com/tracks/189594894/comments?client_id=6ec16ffb5ed930fce00949be480f746b
-url = 'https://api-v2.soundcloud.com/explore/'
-url2 = 'https://api.soundcloud.com/tracks/resolve.json?url=6ec16ffb5ed930fce00949be480f746b&client_id=6ec16ffb5ed930fce00949be480f746b' #just for now
-client_id = '6ec16ffb5ed930fce00949be480f746b'
-client = soundcloud.Client(client_id=client_id, client_secret='1fb20f87e139fcb82f6d856ab6cac086')
+client = soundcloud.Client(client_id=client_id, client_secret=client_secret)
 segmentLen = 3
 
 class Handler(webapp2.RequestHandler):
@@ -53,7 +51,7 @@ class RandomHandler(Handler):
 		if '/' in genre: 
 			genre, sortOption = genre.split('/')
 		else:
-			sortOption = 'random'
+			sortOption = 'hot'				#hot by default
 		arr = []
 		comments = []
 		####
@@ -70,7 +68,7 @@ class RandomHandler(Handler):
 			tracks = json.loads(tracks)
 
 		filter_change_needed = False
-		if str(mc_genre) != str(genre): 		#if memcache needs to update
+		if str(mc_genre) != str(genre): 									#if memcache needs to update bc different genre
 			#url = url + genre + "?limit=50"
 			tracks = json.load(urllib2.urlopen(url)).get('tracks')
 			memcache.set('tracks_'+genre, json.dumps(tracks))
@@ -85,11 +83,14 @@ class RandomHandler(Handler):
 			query = list(query)
 			in_db = False
 			#maybe check created property and update if too old?
+			tooOld = False
 			for q in query:
-				if q.genre == genre:										#if found in db
+				if q.genre == genre:										#if found in db. USE THIS TO IMPLEMENT MULTIPLE GENRE FEATURE
 					in_db = True
+					if time.time() - time.mktime(q.created.timetuple()) > 3600:	#if the db entry is more than an hour old
+						tooOld = True
 					tracks_filtered = json.loads(q.json_str)
-			if not in_db:													#if not in db (and memcache needs to be updated), we send http requests, and then store to db
+			if not in_db or tooOld:												#if not in db or db needs to be updated(and memcache needs to be updated), we send http requests, and then store to db
 				tracks_filtered = []											#going to generate list of track objects
 				for a in range(len(tracks)):
 					if tracks[a].get('streamable') == True and \
@@ -125,7 +126,7 @@ class RandomHandler(Handler):
 								startTime = index
 
 						# how about reddit's hot algorithm? include a hotness attr
-						# hotness value = log(num_likes + 20*num_comments) + time_elapsed/45000
+						# hotness value = log(num_likes * 20*num_comments) + time_elapsed/45000
 						if tracks[a].get('release_day'):
 							time_track = datetime.datetime(tracks[a].get('release_year'), tracks[a].get('release_month'), tracks[a].get('release_day'))
 						else:
@@ -167,7 +168,7 @@ class RandomHandler(Handler):
 		if tracks_filtered and sortOption == 'random': 
 			random.shuffle(tracks_filtered)
 		#or sort based on reddit's hot algorithm?
-		else:
+		elif tracks_filtered:
 			tracks_filtered.sort(key=lambda x: x.get('hotness'), reverse=True)
 		self.write(json.dumps(tracks_filtered))
 		#self.write("This should spit out a random song")
